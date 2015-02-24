173a90b40b48e30ac6a4638d0a90f09a787841c1
jubirosaiko <jubirosaiko@yahoo.co.jp>
2015-02-24 17:35:35 +0900

    Sources

---
+++ Sources/Ball/main.cpp
@@ -0,0 +1,92 @@
+﻿//
+// アプリ雛形
+//
+
+#include "lib/defines.hpp"
+#include "lib/appEnv.hpp"
+
+
+enum Window {
+  WIDTH  = 512,
+  HEIGHT = 512
+};
+
+struct Ball
+{
+	float x;
+	float y;
+	float size_x;
+	float size_y;
+	float r;
+	float speed_x;
+	float speed_y;
+
+}ball;
+
+
+void Init()
+{
+	ball.x = 10;
+	ball.y = 10;
+	ball.size_x = 10;
+	ball.size_y = 10;
+	ball.r = 10;
+	ball.speed_x = 2;
+	ball.speed_y = 3;
+	
+}
+
+float gravity = 0.9;
+
+// 
+// メインプログラム
+// 
+int main() {
+  AppEnv env(Window::WIDTH, Window::HEIGHT, false, false);
+
+
+  Init();
+
+
+
+  while (env.isOpen()) {
+
+	  ball.x += ball.speed_x;
+	  ball.y += ball.speed_y;
+	 
+	  if (ball.x >= WIDTH / 2 - ball.size_x || ball.x <= -WIDTH / 2 + ball.size_x)
+	  {
+		  ball.speed_x *= -1;
+	  }
+	  
+
+
+	  if (ball.y >= HEIGHT / 2 - ball.size_y || ball.y <= -HEIGHT / 2 + ball.size_y)
+	  {
+		  ball.speed_y *= -1;
+		  if (ball.y <= -HEIGHT / 2 + ball.size_y)
+		  {
+			  ball.y = -HEIGHT / 2 + ball.size_y;
+		  }
+		
+	  }
+
+	  if (env.isPressKey('G'))
+	  {
+		  ball.speed_y -= gravity;
+		  ball.y += ball.speed_y;
+	  }
+	  
+
+
+
+
+	  env.setupDraw();
+
+	  drawFillCircle(ball.x, ball.y,ball.size_x, ball.size_y, ball.r, Color(1, 1, 1));
+
+
+
+    env.update();
+  }
+}
---
+++ Sources/Balls/main.cpp
@@ -0,0 +1,116 @@
+﻿//
+// アプリ雛形
+//
+
+#include "lib/defines.hpp"
+#include "lib/appEnv.hpp"
+#include "lib/random.hpp"
+
+enum Window {
+	WIDTH = 512,
+	HEIGHT = 512
+};
+
+enum
+{
+	BALL_MAX = 5
+};
+
+struct Ball
+{
+	float x;
+	float y;
+	float size_x;
+	float size_y;
+	float r;
+	float speed_x;
+	float speed_y;
+	Color color;
+
+}ball[BALL_MAX];
+
+
+Random random;
+
+void Init()
+{
+
+
+	for (int i = 0; i < BALL_MAX; i++)
+	{
+
+		ball[i].size_x = 10;
+		ball[i].size_y = 10;
+		ball[i].r = 10;
+		ball[i].speed_x = 2;
+		ball[i].speed_y = 3;
+		ball[i].x = random.fromFirstToLast(-WIDTH / 2, WIDTH / 2);
+		ball[i].y = random.fromFirstToLast(-HEIGHT / 2, HEIGHT / 2);
+		ball[i].speed_x = random.fromFirstToLast(1, 2);
+		ball[i].speed_y = random.fromFirstToLast(3, 4);
+		ball[i].color = Color(random.fromFirstToLast(0.0f, 1.0f), random.fromFirstToLast(0.0f, 1.0f), random.fromFirstToLast(0.0f, 1.0f));
+	}
+
+
+}
+
+float gravity = 0.9;
+
+
+
+// 
+// メインプログラム
+// 
+int main() {
+	AppEnv env(Window::WIDTH, Window::HEIGHT, false, false);
+
+
+	Init();
+
+
+	while (env.isOpen()) {
+
+
+		for (int i = 0; i < BALL_MAX; i++)
+		{
+			ball[i].x += ball[i].speed_x;
+			ball[i].y += ball[i].speed_y;
+		}
+
+
+		for (int i = 0; i < BALL_MAX; i++)
+		{
+			if (ball[i].x >= WIDTH / 2 - ball[i].size_x || ball[i].x <= -WIDTH / 2 + ball[i].size_x)
+			{
+				ball[i].speed_x *= -1;
+			}
+
+
+
+			if (ball[i].y >= HEIGHT / 2 - ball[i].size_y || ball[i].y <= -HEIGHT / 2 + ball[i].size_y)
+			{
+				ball[i].speed_y *= -1;
+				if (ball[i].y <= -HEIGHT / 2 + ball[i].size_y)
+				{
+					ball[i].y = -HEIGHT / 2 + ball[i].size_y;
+				}
+
+			}
+
+			if (env.isPressKey('G'))
+			{
+				ball[i].speed_y -= gravity;
+				ball[i].y += ball[i].speed_y;
+			}
+		}
+
+		env.setupDraw();
+
+		for (int i = 0; i < BALL_MAX; i++)
+		{
+			drawFillCircle(ball[i].x, ball[i].y, ball[i].size_x, ball[i].size_y, 100, ball[i].color);
+		}
+
+		env.update();
+	}
+}
